b6ebec5ac9b72989fb0f8b0fcc3c8be547f5d55a
mr0x42e <90416856+mr0x42e@users.noreply.github.com>
2022-01-31 19:56:03 +0700

    Upload Mainnet Contracts

--- contracts/AldabraToken.sol
+++ contracts/AldabraToken.sol
@@ -9,7 +9,7 @@ contract AldabraToken is ERC20 {
 
     string private constant name_ = "Aldabra Token";
     string private constant symbol_ = "ALDABRA";
-    uint256 private constant total_supply = 100 * 10**6 * 10**18; // 100m will be mited at genesis for liq pool seeding
+    uint256 private constant total_supply = 100 * 10**6 * 10**18; // 100m will be mited at genesis.
 
     constructor() ERC20(name_, symbol_) public {
         _setupDecimals(18);
